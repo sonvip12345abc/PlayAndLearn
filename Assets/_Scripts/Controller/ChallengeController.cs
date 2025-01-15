0a2ec80271d990ebb941a4c7a014ad1df7e10fb9
LENOVO <sonvip1234a@gmail.com>
2025-01-15 22:23:41 +0700

    commit

--- Assets/_Scripts/Controller/ChallengeController.cs
+++ Assets/_Scripts/Controller/ChallengeController.cs
@@ -51,9 +51,9 @@ public class ChallengeController : Singleton<ChallengeController>
     private void Start()
     {
         listLevelChallenge.Clear();
-        if (listDataLevelChallenge.Count >= 5)
+        if (listDataLevelChallenge.Count >= 25)
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 25; i++)
             {
                 int rd = Random.Range(0, listDataLevelChallenge.Count);
                 listLevelChallenge.Add(listDataLevelChallenge[rd]);
